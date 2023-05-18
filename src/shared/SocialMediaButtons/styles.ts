5a4cf7649bea4470b23556bc06c34212cc441735
WhiteArct1c <matrodrigues1576@gmail.com>
2023-05-18 13:49:02 -0300

    feat: creating and styling the SocialMediaButtons component

---
+++ src/shared/SocialMediaButtons/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Image = styled.figure`
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+
+   position: fixed;
+   right: 3rem;
+   top:14rem;
+
+   //TODO: tooltip for the social media buttons
+
+   img{
+      margin: 1.20rem 0rem;
+      width: 2.15rem;
+   }
+`
