1b88deb7c5adddf9f5b7376aba42b91f75db3f90
WhiteArct1c <matrodrigues1576@gmail.com>
2023-05-18 00:20:06 -0300

    feat: creating global styles

---
+++ src/theme/globalStyles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+   *{
+      padding: 0;
+      margin: 0;
+      border: 0;
+      box-sizing: border-box;
+      list-style: none;
+      font-family: 'Josefin Sans', sans-serif;
+   }
+`
