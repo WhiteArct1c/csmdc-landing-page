0cfc447acffed2f553855a08bcdb9add994701a2
WhiteArct1c <matrodrigues1576@gmail.com>
2023-05-18 00:52:34 -0300

    fix: setting styles for anchors

--- src/shared/navbar/styles.tsx
+++ src/shared/navbar/styles.tsx
@@ -14,12 +14,16 @@ export const Nav = styled.nav`
    background: linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0.63) 52.6%, transparent 100%);
 `
 export const NavButton = styled.li`
-   color: white;
 
    transition: all .3s;
 
    &:hover{
       transform: scale(1.1);
       cursor: pointer;
+   }
+
+   a{
+      text-decoration: none;
+      color: white;
    }
 `
