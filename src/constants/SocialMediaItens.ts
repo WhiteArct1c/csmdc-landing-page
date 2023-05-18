33ae5d9a3889319b2b9cfabecdc5d2ddb1401bea
WhiteArct1c <matrodrigues1576@gmail.com>
2023-05-18 13:50:16 -0300

    feat: adding all the social media infos

---
+++ src/constants/SocialMediaItens.ts
@@ -0,0 +1,22 @@
+export const SocialMediaItens = [
+   {  
+      title: "Discord icon",
+      srcIcon: "/src/assets/discord_button.svg",
+      linkSocialMedia: ""
+   },
+   {
+      title: "Instagram icon",
+      srcIcon: "/src/assets/instagram_button.svg",
+      linkSocialMedia: ""
+   },
+   {
+      title: "Github icon",
+      srcIcon: "/src/assets/github_button.svg",
+      linkSocialMedia: ""
+   },
+   {
+      title: "Twitch icon",
+      srcIcon: "/src/assets/Twitch_button.svg",
+      linkSocialMedia: ""
+   }
+]
