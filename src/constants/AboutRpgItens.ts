5dbf619cecf11356e5a9f83b09b43f00c5078f94
WhiteArct1c <matrodrigues1576@gmail.com>
2023-05-20 17:06:41 -0300

    fix: adjusting grammar

--- src/constants/AboutRpgItens.ts
+++ src/constants/AboutRpgItens.ts
@@ -1,7 +1,7 @@
 export const AboutRpgItens = {
    paragraph: {
       title:  "Sobre o RPG",
-      text: "O Coroa Sombria: Marcas da Convergência é um jogo de RPG de fantasia sombria em que os jogadores assumem o papel de personagens em um mundo repleto de caos e perido em cada esquina. Nesse universo, uma força misteriosa permeia a realidade, permitindo que os usuários realizem mudanças drásticas, mas sempre com um alto preço: o risco de serem consumidos pelo poder que ela concede. Os jogadores enfretarão desafios e perigos, enquanto decidem se devem usar esse poder perigoso para mudar o mundo permanentemente ou se sacrificar tudo para proteger os seres vivos de ameaça que ele representa."
+      text: "O Coroa Sombria: Marcas da Convergência é um jogo de RPG de fantasia sombria em que os jogadores assumem o papel de personagens em um mundo repleto de caos e perigo em cada esquina. Nesse universo, uma força misteriosa permeia a realidade, permitindo que os usuários realizem mudanças drásticas, mas sempre com um alto preço: o risco de serem consumidos pelo poder que ela concede. Os jogadores enfretarão desafios e perigos, enquanto decidem se devem usar esse poder perigoso para mudar o mundo permanentemente ou sacrificar tudo para proteger os seres vivos da ameaça que ele representa."
    },
    image: {
       srcImage: "/src/assets/Figuras-AboutRpg.png",
