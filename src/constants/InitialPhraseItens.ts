83684047c95d7052a3f025436e34e0636fb4cef9
WhiteArct1c <matrodrigues1576@gmail.com>
2023-05-20 03:23:20 -0300

    feat: creating and styling Initial Phrase section

---
+++ src/constants/InitialPhraseItens.ts
@@ -0,0 +1,4 @@
+export const InitialPhraseItens = {
+   text:'Jovem aprendiz, a magia é uma das mais poderosas e magníficas forças do universo. Ela pode moldar a realidade ao nosso redor e nos levar a alturas inimagináveis. Entretanto, é preciso lembrar que toda magia vem com um preço. Em tempos antigos, nós, os magos mais velhos e sábios, testemunhamos o que ficou conhecido como o "Erro do Caos", quando magos imprudentes usaram a magia de forma irresponsável e acabaram por destruir a própria existência. Esse triste episódio nos ensinou que devemos ter respeito pela magia e sempre usá-la com sabedoria e cautela, para não cairmos nos caminhos sombrios e tortuosos que o "Erro do Caos" nos mostrou.',
+   author: "- Aethelred"
+}
