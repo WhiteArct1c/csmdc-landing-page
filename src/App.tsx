129728198becedd43987e2e5879eadd18bef0d33
WhiteArct1c <matrodrigues1576@gmail.com>
2023-05-21 22:00:32 -0300

    refactor: adjusting root constant folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { MenuItens } from "./constants/MenuItens";
-import { SocialMediaItens } from "./constants/SocialMediaItens";
-import { InitialPhraseItens } from "./constants/InitialPhraseItens";
-import { AboutRpgItens } from "./constants/AboutRpgItens";
+import { MenuItens } from "./types/constants/MenuItens";
+import { SocialMediaItens } from "./types/constants/SocialMediaItens";
+import { InitialPhraseItens } from "./types/constants/InitialPhraseItens";
+import { AboutRpgItens } from "./types/constants/AboutRpgItens";
 
 import { HomePage } from "./pages/HomePage";
 import { InitialPhrase } from "./pages/InitialPhrase";
@@ -11,7 +11,7 @@ import { SocialMediaButtons } from "./shared/SocialMediaButtons";
 
 import { GlobalStyle } from "./theme/globalStyles";
 import { SectionTextImage } from "./shared/Section[Text,Image]";
-import { OurProduct } from "./constants/OurProduct";
+import { OurProduct } from "./types/constants/OurProduct";
 
 function App() {
 
