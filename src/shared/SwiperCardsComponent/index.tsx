86d72b71c9e3a354afb6f7dff21932089c1eb2b0
WhiteArct1c <matrodrigues1576@gmail.com>
2023-07-01 11:58:42 -0300

    feat: adjusting the indentation

--- src/shared/SwiperCardsComponent/index.tsx
+++ src/shared/SwiperCardsComponent/index.tsx
@@ -14,43 +14,42 @@ import AOS from 'aos';
 export const SwiperComponent: React.FC<SwiperComponentProps> = ({profiles}) => {
    register();
    AOS.init();
+
    return (
       <CardsContainer
          data-aos='fade-right'
          data-aos-duration="2000"
       >
          <Swiper
-        effect={"cards"}
-        grabCursor={true}
-        modules={[EffectCards]}
-        className="mySwiper"
-      >
-        {
-         profiles.map(profile => {
-            return(
-               <>
-               <SwiperSlide key={profile.id}>
-                  <CardContent>
-                     <Avatar src={profile.urlAvatar} alt={`Foto perfil de ${profile.name}`}/>
-                     <hr/>
-                     <ProfileName>{profile.name}</ProfileName>
-                     <Role>{profile.role}</Role>
-                     <SocialMediaContainer>
-                        {profile.socialMedia.map(pSocialMedia => {
-                           return (                             
-                              <a href={pSocialMedia.link} target="_blank" rel="noreferrer">
-                                 <img src={pSocialMedia.urlIcon} alt={pSocialMedia.title}/>
-                              </a>                              
-                           );
-                        })}
-                     </SocialMediaContainer>
-                  </CardContent>
-               </SwiperSlide>
-               </>
-            );
-         })
-        }
-      </Swiper>
+            effect={"cards"}
+            grabCursor={true}
+            modules={[EffectCards]}
+            className="mySwiper"
+         >
+         {
+            profiles.map((profile, index) => {
+               return(
+                  <SwiperSlide key={index}>
+                     <CardContent>
+                        <Avatar src={profile.urlAvatar} alt={`Foto perfil de ${profile.name}`}/>
+                        <hr/>
+                        <ProfileName>{profile.name}</ProfileName>
+                        <Role>{profile.role}</Role>
+                        <SocialMediaContainer>
+                           {profile.socialMedia.map((pSocialMedia, index) => {
+                              return (                             
+                                 <a href={pSocialMedia.link} target="_blank" rel="noreferrer" key={index}>
+                                    <img src={pSocialMedia.urlIcon} alt={pSocialMedia.title}/>
+                                 </a>                              
+                              );
+                           })}
+                        </SocialMediaContainer>
+                     </CardContent>
+                  </SwiperSlide>
+               );
+            })
+         }
+         </Swiper>
       </CardsContainer>
    );
 };
